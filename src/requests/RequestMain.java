c4d76d69217aee65904b0b2ce5adc67f0b35775e
Bill Than <bill.than@mail.utoronto.ca>
2020-07-25 11:41:29 -0400

    Restructuring HTMLRequest

--- src/requests/RequestMain.java
+++ src/requests/RequestMain.java
@@ -1,4 +1,8 @@
 package requests;
+import java.io.IOException;
+
+import org.json.JSONException;
+
 import requests.apiRequests.darksky.DarkSkyRequest;
 import requests.apiRequests.tomtom.TomTomRequest;
 
@@ -19,11 +23,22 @@ public class RequestMain {
 		RequestMain.d = new DarkSkyRequest();
 	}
 
-	public static void main(String args[]) {
+	public static void main(String args[]) throws JSONException, IOException {
+		/**
 		RequestMain r = new RequestMain("165A Sheldon Avenue");
 		System.out.println("tomtom " + t.key);
 		System.out.println("darksky " + d.key);
+			**/
 
+		try {
+			HTMLRequest h = new HTMLRequest("https://api.darksky.net/forecast/54014a5f81c8b8d34fb16a5f669ba9f1/37.8267,-122.4233");
+		} catch (JSONException e) {
+			// TODO Auto-generated catch block
+			e.printStackTrace();
+		} catch (IOException e) {
+			// TODO Auto-generated catch block
+			e.printStackTrace();
+		}
 	}
 
 }
